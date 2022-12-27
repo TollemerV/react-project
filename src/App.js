9fb69e941fdf08a71be19cc96d54a2275fa2f2fe
Ayoub-Tehari <ayoub.tehari01@gmail.com>
2022-12-27 12:56:20 +0100

    Reservation view : OK

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from 'styled-components';
 import {getTheme} from './themes/default.js';
 import NoPage from './components/templates/NoPage/NoPage';
 import Reservation from './components/templates/Reservation/Reservation';
+import ReservationView from './components/templates/ReservationView/ReservationView';
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -13,6 +14,7 @@ export default function App() {
             <Router>
               <Routes>
                 <Route path={"/reservation"} element={<Reservation/>}/>
+                <Route path={"/ReservationView"} element={<ReservationView/>}/>
                 <Route path="*" element={<NoPage />} />
               </Routes>
             </Router>
---
+++ src/components/templates/ReservationView/ReservationView.js
@@ -0,0 +1,36 @@
+
+import * as React from 'react';
+import "./reservationView.css";
+import { getData } from '../../../services/getData';
+
+
+
+function ReservationView (){
+    
+     const [reservations, setReservations] = React.useState([]);
+  
+  
+
+  const getD = async () => {
+    const response = await getData("reservation");
+
+    setReservations([...reservations, ...response.data]);
+  };
+  React.useEffect(() => {
+    getD();
+  },[]);
+    return <div className="wrapper" ><table>
+            <tbody>
+                <tr>
+                    <th>Id</th>
+                    <th>Numéro de table</th>
+                    <th>Heure</th>
+                    <th>Date</th>
+                </tr>
+                {reservations.map((e)=><tr key={e.id+"-tr"} ><td key={e.id+"-1"}>{e.id}</td><td key={e.id+"-2"}>{e.numTable}</td><td key={e.id+"-3"}>{e.heure}</td><td key={e.id+"-4"}>{e.date}</td></tr>)}
+            </tbody>
+            </table>
+            </div>;
+}
+
+export default ReservationView;
