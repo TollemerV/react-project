16c084279003b6b23daaf39c80d88010b58daf26
Lucas Bento-Versace <lucas.bentoversace@ynov.com>
2023-01-02 13:44:34 +0100

    add img for menu, add carousel for home, fix css on menu and home (#7)

--- src/components/templates/Cuisine/Cuisine.js
+++ src/components/templates/Cuisine/Cuisine.js
@@ -1,50 +1,60 @@
 import "./cuisine.css";
 import NavBar from "../../molecules/NavBar/NavBar";
 import * as React from 'react';
-import clock from '../../../assets/clock.png';
+import inventory from '../../../assets/inventaire.png';
+import command from '../../../assets/commande.png';
+import history from '../../../assets/history.png';
+import menu from '../../../assets/menu.png';
 import CardCuisine from "../../atoms/CardCuisine/CardCuisine";
 
 export default function Cuisine () {
   
     return (<>
-                     <div id="container-cuisine-1">
-                            <span id="cuisine-title">Cuisine</span>
-                            <div id="container-cuisine-2">
-                                   <CardCuisine
-                                          img={clock} 
-                                          imgId="img1-inventaire" 
-                                          imgAlt="inventaire" 
-                                          text="Inventaire" 
-                                          to="/inventaire">
 
-                                   </CardCuisine>
-                                   <CardCuisine 
-                                          img={clock} 
-                                          imgId="img1-cmds" 
-                                          imgAlt="commandes à effectuer" 
-                                          text="Commandes à effectuer" 
-                                          to="/cmd">
+<div id="container-cuisine-1" className="container">
 
-                                   </CardCuisine>
-                                   <CardCuisine 
-                                          img={clock} 
-                                          imgId="img1-histo-cmds" 
-                                          imgAlt="historique de commandes" 
-                                          text="Historique de commandes" 
-                                          to="/histo-cmd">
+       <h1 id="cuisine-title">Cuisine</h1>
+       <div id="container-cuisine-2">
+              <div className="row">
+                     <CardCuisine
+                            img={inventory} 
+                            imgId="img1-inventaire" 
+                            imgAlt="inventaire" 
+                            text="Inventaire" 
+                            to="/inventaire">
 
-                                   </CardCuisine>
-                                   <CardCuisine 
-                                          img={clock} 
-                                          imgId="img1-menu" 
-                                          imgAlt="menu" 
-                                          text="Menu"  
-                                          to="/menu">
+                     </CardCuisine>
+                     <CardCuisine 
+                            img={command} 
+                            imgId="img1-cmds" 
+                            imgAlt="commandes à effectuer" 
+                            text="Commandes à effectuer" 
+                            to="/cmd">
 
-                                   </CardCuisine>
-                            </div>
-            
-                     </div>
-                     <NavBar></NavBar>
+                     </CardCuisine>
+              </div>
+              <div className="row">
+                     <CardCuisine 
+                            img={history} 
+                            imgId="img1-histo-cmds" 
+                            imgAlt="historique de commandes" 
+                            text="Historique de commandes" 
+                            to="/histo-cmd">
+
+                     </CardCuisine>
+                     <CardCuisine 
+                            img={menu} 
+                            imgId="img1-menu" 
+                            imgAlt="menu" 
+                            text="Menu"  
+                            to="/menu">
+
+                     </CardCuisine>
+              </div>
+
+       </div>
+
+</div>
+<NavBar></NavBar>
             </>);
 }
